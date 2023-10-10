c66bb8ea3706ba24e57a90cf8778143c819e64a4
Alex Balmus <alexandru.balmus.ext@porscheinformatik.com>
2023-10-10 14:33:05 +0300

    Updates

--- src/main/java/com/alexbalmus/HibernateExample.java
+++ src/main/java/com/alexbalmus/HibernateExample.java
@@ -2,27 +2,21 @@ package com.alexbalmus;
 
 import static java.lang.Boolean.TRUE;
 import static java.lang.System.out;
-import static org.hibernate.cfg.AvailableSettings.*;
-
-import java.util.Map;
-import java.util.function.Consumer;
+import static org.hibernate.cfg.AvailableSettings.FORMAT_SQL;
+import static org.hibernate.cfg.AvailableSettings.HIGHLIGHT_SQL;
+import static org.hibernate.cfg.AvailableSettings.PASS;
+import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
+import static org.hibernate.cfg.AvailableSettings.URL;
+import static org.hibernate.cfg.AvailableSettings.USER;
 
 import org.hibernate.cfg.Configuration;
 
 import com.alexbalmus.jpapractice.Book;
 import com.alexbalmus.jpapractice.Book_;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-
-public class Main
+public class HibernateExample
 {
     public static void main(String[] args)
-    {
-        performWithHibernateApi();
-    }
-
-    private static void performWithHibernateApi()
     {
         var sessionFactory = new Configuration()
             .addAnnotatedClass(Book.class)
@@ -69,48 +63,4 @@ public class Main
         });
     }
 
-    private static void performWithJPAApi()
-    {
-        var factory = createEntityManagerFactory("example",
-            // export the inferred database schema
-            Map.of(JAKARTA_HBM2DDL_DATABASE_ACTION, CREATE));
-
-        // persist an entity
-        inSession(factory, entityManager -> {
-            entityManager.persist(new Book("9781932394153", "Hibernate in Action"));
-        });
-
-        // query data using HQL
-        inSession(factory, entityManager -> {
-            out.println(entityManager.createQuery("select isbn||': '||title from Book").getSingleResult());
-        });
-
-        // query data using criteria API
-        inSession(factory, entityManager -> {
-            var builder = factory.getCriteriaBuilder();
-            var query = builder.createQuery(String.class);
-            var book = query.from(Book.class);
-            query.select(builder.concat(builder.concat(book.get(Book_.isbn), builder.literal(": ")),
-                book.get(Book_.title)));
-            out.println(entityManager.createQuery(query).getSingleResult());
-        });
-    }
-
-    // do some work in a session, performing correct transaction management
-    static void inSession(EntityManagerFactory factory, Consumer<EntityManager> work) {
-        var entityManager = factory.createEntityManager();
-        var transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            work.accept(entityManager);
-            transaction.commit();
-        }
-        catch (Exception e) {
-            if (transaction.isActive()) transaction.rollback();
-            throw e;
-        }
-        finally {
-            entityManager.close();
-        }
-    }
 }
--- src/main/java/com/alexbalmus/jpapractice/Book.java
+++ src/main/java/com/alexbalmus/jpapractice/Book.java
@@ -1,2 +1,25 @@
-package com.alexbalmus.jpapractice;public class Book {
+package com.alexbalmus.jpapractice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Book
+{
+    @Id
+    String isbn;
+
+    @NotNull
+    String title;
+
+    protected Book()
+    {
+    }
+
+    public Book(String isbn, String title)
+    {
+        this.isbn = isbn;
+        this.title = title;
+    }
 }
